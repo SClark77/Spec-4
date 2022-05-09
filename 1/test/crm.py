d75bea8a04cdd072c3633303d77f8706631a2cb8
SClark77 <88445154+SClark77@users.noreply.github.com>
2022-05-09 09:21:26 -0600

    Add files via upload

---
+++ 1/test/crm.py
@@ -0,0 +1,45 @@
+############################################################
+# Customer Relationship Database using Python and MySql.   #
+# Lesson From Codemy.com                                   #
+#                                                          #
+# Student = Stephen Clark 2022                             #
+#                                                          #
+############################################################
+
+'''
+head over to mysql.com
+Download Community Edition ( unable to install at work)
+Mysql installer for windows
+Download the larger file
+Run Installer
+Create Password
+Configure as windows service and start when windows starts
+...
+
+pip install mysql-connector
+
+pip install mysql-connector-python
+
+pip install mysql-connector-python-rf
+
+...
+
+
+
+'''
+
+from tkinter import *
+from PIL import ImageTk,Image
+import mysql.connector
+
+root = Tk()
+root.title('CRM from Codemy lesson')
+root.iconbitmap('spec4.ico')
+root.geometry("400x800")
+
+
+
+
+root.mainloop()
+
+
